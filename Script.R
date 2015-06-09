f9dc426e1b2176e57ddc5cae493fa0ef15e0a5f7
Diego Huaraca <diego.21.phs@hotmail.com>
2015-06-09 10:08:28 -0500

    Script Execution

--- Script.R
+++ Script.R
@@ -8,7 +8,13 @@ library(rmarkdown)
 dir.main <- "/Users/Diego/Dropbox/EPN/Libro Series"
 dir.cap <- "/Users/Diego/Dropbox/EPN/Libro Series/Capitulos"
 
+#### Capitulos ####
+setwd(dir.cap)
+file.create("Cap5.tex")
+file.edit("Cap5.tex")
 
+
+#### Execution ####
 system("xelatex -interaction=batchmode book_series ")
 system("makeindex book_series")
 system("xelatex -interaction=batchmode book_series ")
