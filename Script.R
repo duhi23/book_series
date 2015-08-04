d2efabce73fa1dd661354718e1aa9832cda76712
duhi23 <diego.21.phs@hotmail.com>
2015-08-04 14:11:13 -0500

    Update

--- Script.R
+++ Script.R
@@ -11,12 +11,10 @@ dir.cap <- "/Users/Diego/Dropbox/EPN/Libro Series/Capitulos"
 dir.main <- "/Users/usuario4/Dropbox/EPN/Libro Series"
 dir.cap <- "/Users/usuario4/Dropbox/EPN/Libro Series/Capitulos"
 
-
 #### Capitulos ####
 setwd(dir.cap)
 file.create("Cap7.tex")
 file.edit("Cap7.tex")
-
 
 #### Execution ####
 system("xelatex -interaction=batchmode book_series ")
